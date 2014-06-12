c83a41c2f309caf230d9a9eed1a5769391704225
George Czabania <george@czabania.com>
2014-06-12 19:48:07 +1200

    Throw error if buffers are used

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var through = require('through2');
 module.exports = function () {
 
   return through.obj(function (file, enc, callback) {
+    if (file.isBuffer()) {
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Buffers not supported!'));
+      return callback();
+    }
+
     if (file.isStream()) {
       var stream = brfs(file.path);
       file.contents = file.contents.pipe(stream);
