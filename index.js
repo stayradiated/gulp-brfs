e4e5ffe33c295ce57e322527808967cd4cd8d778
SevereOverfl0w <overfl0w@narna.co>
2014-11-14 17:24:49 +0000

    Actuall add buffer support. Derp.

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 var brfs = require('brfs');
 var through = require('through2');
+var bl = require('bl');
 
 module.exports = function () {
 
   return through.obj(function (file, enc, callback) {
+    var stream = brfs(file.path);
     if (file.isBuffer()) {
-      file.contents = new Buffer(file.pipe(brfs(file.path)));
+      var that = this;
+      file.pipe(stream)
+          .pipe(bl(function(err, data){
+            file.contents = data;
+            that.push(file);
+            return callback();
+          }));
     }
 
     if (file.isStream()) {
-      var stream = brfs(file.path);
       file.contents = file.contents.pipe(stream);
     }
 
